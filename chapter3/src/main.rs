3a0e306c46a2ba49c0b02b6dd0039b7913ffe03f
CryptoSnowPrince <topdirector2017@gmail.com>
2023-01-10 09:43:08 -0500

    complete chapter 3

---
+++ chapter3/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    for number in (3..10).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+    for number in 3..10 {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
